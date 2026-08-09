0bfee904e5b90d84d4ac247ee21a7d39961efdea
THARA HUOKAING <tharahuokaing@gmail.com>
2026-08-09 17:06:31 +0700

    Update login1.js

--- login1.js
+++ login1.js
@@ -208,7 +208,7 @@
                 <div class="dashboard-card">
                     <a href="https://tharahuokaing.github.io/transaction/" style="text-decoration: none; color: inherit; display: block; height: 100%; width: 100%;">
                         <img src="transaction_volumn.jpg" alt="Transactions Icon" class="card-icon" style="width:150px; height:150px;">
-                        <h3 id="txCount">$400,00</h3>
+                        <h3 id="txCount">$400,000</h3>
                         <p>Transactions Processed</p>
                         <span class="card-link">View Log</span>
                     </a>
