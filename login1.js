6b97f2d8000862beb70c9c57a8fb0ea23c877431
THARA HUOKAING <tharahuokaing@gmail.com>
2026-08-10 08:11:28 +0700

    Update total deposit amount to $420,000,000

--- login1.js
+++ login1.js
@@ -166,7 +166,7 @@
         log("Dashboard active. Injecting financial records into DOM nodes...");
         if (document.getElementById("accountCount")) {
             document.getElementById("accountCount").textContent = "$250,000<";
-            document.getElementById("depositTotal").textContent = "$410,000,000";
+            document.getElementById("depositTotal").textContent = "$420,000,000";
             document.getElementById("txCount").textContent = "$0";
         }
     }
@@ -198,7 +198,7 @@
                 <div class="dashboard-card">
                     <a href="https://tharahuokaing.github.io/deposit/" style="text-decoration: none; color: inherit; display: block; height: 100%; width: 100%;">
                         <img src="total_deposit.jpg" alt="Deposits Icon" class="card-icon" style="width:150px; height:150px;">
-                        <h3 id="depositTotal">$410,000,000</h3>
+                        <h3 id="depositTotal">$420,000,000</h3>
                         <p>Total Deposits</p>
                         <span class="card-link">View Details</span>
                     </a>
